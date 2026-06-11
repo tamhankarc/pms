9a3eb083c3bd33195c9a1c287b02adb43102b2f3
tamhankarc <chaitanyat@sycamoresol.com>
2026-06-11 06:30:35 +0000

    Update 105 - minor change 5

--- lib/permissions.ts
+++ lib/permissions.ts
@@ -281,6 +281,7 @@ export function canAccessLeaveRequests(user: UserLike) {
 export function canManageManualAttendance(user: UserLike) {
   return (
     (isAdmin(user) && getFunctionalRole(user) === "OTHER") ||
+    isHR(user) ||
     (isAdmin(user) && getFunctionalRole(user) === "PROJECT_MANAGER") ||
     hasExtraMenuAccess(user, "attendance-history")
   );
